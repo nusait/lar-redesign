167fd178c386d3eb650402917a6154ca15f381ed
Christopher E. Walker <cewalker@uchicago.edu>
2014-01-04 19:41:20 -0600

    uploaded new cascade template to `Cascade/newest-template/` folder, rebuilt js as build-mi.js

--- public/js/build.js
+++ public/js/build.js
@@ -13867,6 +13867,9 @@ define('mobilemenu',['jquery', 'hammer'], function ($, Hammer)
     {
         var MobileMenu = {
             initialize: function () {
+                $('.menu-drawer').on('click', function () {
+                    $('html').toggleClass('show-drawer');
+                });
                 $('body').one('touchstart', function() {
                     $('.middle').hammer().on('dragup swipeup', this, function (ev) {
                         $('html').removeClass('show-top');
