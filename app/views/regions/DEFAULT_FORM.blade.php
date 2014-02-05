91fd8900436c05f6f4b4a812af15cb723afb1f3a
Hao Luo <howlowck@gmail.com>
2014-02-05 15:32:59 -0600

    fixed form content type

--- app/views/regions/DEFAULT_FORM.blade.php
+++ app/views/regions/DEFAULT_FORM.blade.php
@@ -1,2 +1,6 @@
 <div class="title-lvl-2-container"><h3 class="title-lvl-2">Some Form</h3></div>
-<div class="machform" data-id="14117"></div>
+<div class="basic-container">
+	<div class="basic-text-container">
+		<div class="machform" data-id="14117"></div>
+	</div>
+</div>
