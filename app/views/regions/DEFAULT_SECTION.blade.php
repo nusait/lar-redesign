c308e3a2d50359398744d07d213cadd241402986
Hao Luo <howlowck@gmail.com>
2014-02-06 11:38:29 -0600

    added section style, updated region DEFAULT_SECTION

--- app/views/regions/DEFAULT_SECTION.blade.php
+++ app/views/regions/DEFAULT_SECTION.blade.php
@@ -1,50 +1,54 @@
 <div class="title-lvl-2-container"><h3 class="title-lvl-2">Some Form</h3></div>
 <div class="basic-container">
-	<div class="basic-heading-container">
-		<h2>Some Heading</h2>
-	</div>
-	<div class="basic-image-container">
-		<div class="basic-image-item">
-			<img alt="a placeholder image of height and width 600 pixels" src="http://placehold.it/600x600"/>
-			<div class="basic-image-caption">
-				<p>You'll want to swap in your own photo here - No Link</p>
+	<div class="section-container">
+		<div class="basic-heading-container">
+			<h2>Some Heading</h2>
+		</div>
+		<div class="basic-image-container">
+			<div class="basic-image-item">
+				<img alt="a placeholder image of height and width 600 pixels" src="http://placehold.it/600x600"/>
+				<div class="basic-image-caption">
+					<p>You'll want to swap in your own photo here - No Link</p>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="basic-text-container">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi odio mauris, auctor ut varius non, tempus nec nisi. Quisque at tellus risus. Aliquam erat volutpat. Proin et dolor magna. Sed vel metus justo. Mauris eu metus massa. Duis viverra ultricies nisl, ut pharetra eros hendrerit non. Phasellus laoreet libero non eros rhoncus sed iaculis ante molestie. Etiam arcu purus, dictum a tincidunt id, ornare sed orci. Curabitur ornare ornare nulla quis tincidunt. Morbi nisi elit, mattis nec bibendum vel, facilisis eu ipsum. Phasellus non dolor erat, in placerat lacus. Aliquam pulvinar, est eu commodo vulputate, neque elit molestie lorem, sed vestibulum felis erat et risus. Nulla non velit odio.
-		<ol>
-			<li>hi</li>
-			<li>this</li>
-			<li>is</li>
-			<li>a</li>
-			<li>list</li>
-			<li>sadd</li>
-			<li>dfsdf</li>
-			<li>fdsf</li>
-			<li>sdf</li>
-			<li>sdfd</li>
-		</ol>
-	</div>
-	<div class="basic-heading-container">
-		<h2>Some Heading</h2>
-	</div>
-	<div class="basic-image-container">
-		<div class="basic-image-item">
-			<img alt="a placeholder image of height and width 600 pixels" src="http://placehold.it/600x600"/>
-			<div class="basic-image-caption">
-				<p>You'll want to swap in your own photo here - No Link</p>
-			</div>
+		<div class="basic-text-container">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi odio mauris, auctor ut varius non, tempus nec nisi. Quisque at tellus risus. Aliquam erat volutpat. Proin et dolor magna. Sed vel metus justo. Mauris eu metus massa. Duis viverra ultricies nisl, ut pharetra eros hendrerit non. Phasellus laoreet libero non eros rhoncus sed iaculis ante molestie. Etiam arcu purus, dictum a tincidunt id, ornare sed orci. Curabitur ornare ornare nulla quis tincidunt. Morbi nisi elit, mattis nec bibendum vel, facilisis eu ipsum. Phasellus non dolor erat, in placerat lacus. Aliquam pulvinar, est eu commodo vulputate, neque elit molestie lorem, sed vestibulum felis erat et risus. Nulla non velit odio.
+			<ol>
+				<li>hi</li>
+				<li>this</li>
+				<li>is</li>
+				<li>a</li>
+				<li>list</li>
+				<li>sadd</li>
+				<li>dfsdf</li>
+				<li>fdsf</li>
+				<li>sdf</li>
+				<li>sdfd</li>
+			</ol>
 		</div>
 	</div>
-	<div class="basic-text-container">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi odio mauris, auctor ut varius non, tempus nec nisi. Quisque at tellus risus. Aliquam erat volutpat. Proin et dolor magna. Sed vel metus justo. Mauris eu metus massa. Duis viverra ultricies nisl, ut pharetra eros hendrerit non. Phasellus laoreet libero non eros rhoncus sed iaculis ante molestie. Etiam arcu purus, dictum a tincidunt id, ornare sed orci. Curabitur ornare ornare nulla quis tincidunt. Morbi nisi elit, mattis nec bibendum vel, facilisis eu ipsum. Phasellus non dolor erat, in placerat lacus. Aliquam pulvinar, est eu commodo vulputate, neque elit molestie lorem, sed vestibulum felis erat et risus. Nulla non velit odio.
-		<ol>
-			<li>hi</li>
-			<li>this</li>
-			<li>is</li>
-			<li>a</li>
-			<li>list</li>
-		</ol>
+	<div class="section-container">
+		<div class="basic-heading-container">
+			<h2>Some Heading</h2>
+		</div>
+		<div class="basic-image-container">
+			<div class="basic-image-item">
+				<img alt="a placeholder image of height and width 600 pixels" src="http://placehold.it/600x600"/>
+				<div class="basic-image-caption">
+					<p>You'll want to swap in your own photo here - No Link</p>
+				</div>
+			</div>
+		</div>
+		<div class="basic-text-container">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi odio mauris, auctor ut varius non, tempus nec nisi. Quisque at tellus risus. Aliquam erat volutpat. Proin et dolor magna. Sed vel metus justo. Mauris eu metus massa. Duis viverra ultricies nisl, ut pharetra eros hendrerit non. Phasellus laoreet libero non eros rhoncus sed iaculis ante molestie. Etiam arcu purus, dictum a tincidunt id, ornare sed orci. Curabitur ornare ornare nulla quis tincidunt. Morbi nisi elit, mattis nec bibendum vel, facilisis eu ipsum. Phasellus non dolor erat, in placerat lacus. Aliquam pulvinar, est eu commodo vulputate, neque elit molestie lorem, sed vestibulum felis erat et risus. Nulla non velit odio.
+			<ol>
+				<li>hi</li>
+				<li>this</li>
+				<li>is</li>
+				<li>a</li>
+				<li>list</li>
+			</ol>
+		</div>
 	</div>
 </div>
