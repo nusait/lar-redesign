1b9f1c84a595887bc7c3356aa6a22cad1a997da0
Hao Luo <howlowck@gmail.com>
2014-02-14 16:15:24 -0600

    added vimeo to lar-redesign

--- app/views/regions/DEFAULT_VIDEO.blade.php
+++ app/views/regions/DEFAULT_VIDEO.blade.php
@@ -5,7 +5,8 @@
 			Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.
 		</div>
 		<div class="video-wrapper">
-			<iframe src="//www.youtube.com/embed/LE2v3sUzTH4?rel=0" frameborder="0" allowfullscreen></iframe>
+			<!-- <iframe src="//www.youtube.com/embed/LE2v3sUzTH4?rel=0" frameborder="0" allowfullscreen></iframe> -->
+			<iframe src="//player.vimeo.com/video/74904021" frameborder="0" allowfullscreen></iframe>
 		</div>
 		<div class="after-text">
 			Bacon ipsum dolor sit amet beef shoulder frankfurter brisket short loin. Capicola shankle pork belly, turducken chuck doner leberkas short loin. Boudin strip steak pork loin shankle flank spare ribs shoulder. Ball tip leberkas beef shank jerky beef ribs tongue capicola short loin pork belly filet mignon ribeye pork doner.
