3ed7b291a447f510884eeae73731af0e48b4a3e3
Hao Luo <howlowck@gmail.com>
2014-04-01 12:39:24 -0500

    changed footer

--- app/views/regions/FOOTER.blade.php
+++ app/views/regions/FOOTER.blade.php
@@ -3,12 +3,17 @@
     <div class="arrow-button" aria-label="open footer"></div>
     <div class="details">
       <div class="department-name">
-        Asian/Asian American Student Affairs
-      </div><div class="divider"></div><!--
-      --><div class="address">
+        Asian/Asian American Student Affairs <br/>
         Rebecca Crown Evanston, IL 60208 <br/>
         <a href="mailto:v.redmond@northwestern.edu">v.redmond@northwestern.edu</a> <br/>
         Phone: 847-491-5360
+      </div><div class="divider"></div><!--
+      --><div class="address">
+        <li><a href="">Dosa Home</a></li>
+        <li><a href="">Dosa Events</a></li>
+        <li><a href="">Map</a></li>
+        <li><a href="">Dosa Events</a></li>
+        <li><a href="">Map</a></li>
       </div><div class="divider"></div><!--
       --><div class="dosa-links">
         <li><a href="">Dosa Home</a></li>
@@ -24,7 +29,7 @@
         <li><a href="http://www.northwestern.edu/studentaffairs/">Student Affairs Home</a></li>
         <li><a href="http://www.northwestern.edu">Northwestern Home</a></li>
         <li><a href="#events">Plan-It Purple</a></li>
-        <li><a href="#map">Campus Map</a></li>
+        <li><a href="#">Department Events</a></li>
       </ul>
       <div class="social">
         <a class="facebook" href="http://www.facebook.com/studentaffnu" aria-label="Facebook">Facebook</a>
