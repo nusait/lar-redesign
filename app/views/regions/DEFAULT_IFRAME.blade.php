b7dd36dc8ca001dfe480adeb8e759640c1b5f55e
Hao Luo <howlowck@gmail.com>
2015-04-01 14:04:34 -0500

    changed iframe

--- app/views/regions/DEFAULT_IFRAME.blade.php
+++ app/views/regions/DEFAULT_IFRAME.blade.php
@@ -1,4 +1,12 @@
 <div class="title-lvl-2-container"><h3 class="title-lvl-2">Panorama!</h3></div>
-<div class="iframe-container">
-  <iframe class="sa-iframe" data-background="#eff1f1" height="530px" width="100%" src="http://go.dosa.northwestern.edu/shared/panofiles/output/ElderDouble.html"></iframe>
+<div class="basic-container">
+  <div class="basic-text-container">
+    <div class="before-text">
+      Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.
+    </div>
+    <iframe class="sa-iframe" height="530px" width="100%" src="http://go.dosa.northwestern.edu/shared/panofiles/output/ElderDouble.html"></iframe>
+    <div class="after-text">
+      Bacon ipsum dolor sit amet beef shoulder frankfurter brisket short loin. Capicola shankle pork belly, turducken chuck doner leberkas short loin. Boudin strip steak pork loin shankle flank spare ribs shoulder. Ball tip leberkas beef shank jerky beef ribs tongue capicola short loin pork belly filet mignon ribeye pork doner.
+    </div>
+  </div>
 </div>
